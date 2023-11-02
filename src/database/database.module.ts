5b3537003d82962f39b79041130e1de8bd89134e
Viachaslau Kulbitski <fixrapdok@gmail.com>
2023-11-02 22:29:28 +0300

    update: env props

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.d83zfqn.mongodb.net/DoJewerly',
-    ),
+    ConfigModule.forRoot(), // Убедитесь, что загружен ConfigModule
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'), // Используйте ConfigService для получения переменной окружения
+      }),
+      inject: [ConfigService],
+    }),
   ],
 })
 export class DatabaseModule {}
